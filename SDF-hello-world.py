9673dc8f9832271f5574c36b306158cd0daad9ba
kevinmarkoshaughnessy-blip <kevin.mark.oshaughnessy@gmail.com>
2026-03-18 22:12:50 +0000

    Update SDF-hello-world.py
    XL model. This is 14GB. Older model is 4GB.

--- SDF-hello-world.py
+++ SDF-hello-world.py
@@ -1,10 +1,21 @@
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionXLPipeline
 import torch
 
-print("Loading stable diffusion...")
-pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+pipe = StableDiffusionXLPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", 
+    torch_dtype=torch.float16 
+)
 print("Pipeline loaded on GPU")
 
-image = pipe("a photo of a beautiful woman's smiling face, portrait, facing forward, no teeth showing").images[0]
+# for the full list of parameters see https://huggingface.co/docs/diffusers/api/pipelines/stable_diffusion/stable_diffusion_xl#diffusers.StableDiffusionXLPipeline.__call__
+image = pipe("a photo of a beautiful latina woman's face, portrait, facing forward, no teeth showing",
+            num_inference_steps=30,      # more steps = higher quality, slower
+            guidance_scale=20,          # how closely to follow the prompt (higher = more literal)
+            negative_prompt="blurry, distorted, low quality",  # what to avoid
+            num_images_per_prompt = 1,
+            seed=1                      # for reproducibility
+            ).images[0]
 image.save("test_face.png")
+
+print("Image saved")
+image
