5345d2cbf8ae686324bedd0e77faa958c86a0eb9
kevinmarkoshaughnessy-blip <kevin.mark.oshaughnessy@gmail.com>
2026-03-19 06:11:28 +0000

    Update SDF-hello-world.py
    Apprarently the XL model can't produce good 256x256 images but so generate 512x512 and then resize.

--- SDF-hello-world.py
+++ SDF-hello-world.py
@@ -11,12 +11,16 @@ print("Pipeline loaded on GPU")
 
 # for the full list of parameters see https://huggingface.co/docs/diffusers/api/pipelines/stable_diffusion/stable_diffusion_xl#diffusers.StableDiffusionXLPipeline.__call__
 image = pipe("a photo of a beautiful latina woman's face, portrait, facing forward, no teeth showing",
-            num_inference_steps=30,      # more steps = higher quality, slower
-            guidance_scale=20,          # how closely to follow the prompt (higher = more literal)
+            num_inference_steps=100,      # more steps = higher quality, slower
+            guidance_scale=15,          # how closely to follow the prompt (higher = more literal)
             negative_prompt="blurry, distorted, low quality",  # what to avoid
             num_images_per_prompt = 1,
+            height = 512,
+            width = 512,
             seed=1                      # for reproducibility
             ).images[0]
+
+image = image.resize((256, 256))
 image.save("test_face.png")
 
 print("Image saved")
